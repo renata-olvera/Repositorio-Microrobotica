bd3c73385f7fe7838bb822bfaf7045ab6c9757a8
renata-olvera1 <a23310301@live.ceti.mx>
2026-09-18 23:47:46 -0600

    final
    proyecto completo

---
+++ controllers/controlador_pendulo/build/release/controlador_pendulo.d
@@ -0,0 +1,6 @@
+build/release/controlador_pendulo.o: controlador_pendulo.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
--- controllers/controlador_pendulo/controlador_pendulo.cpp
+++ controllers/controlador_pendulo/controlador_pendulo.cpp
@@ -2,43 +2,29 @@
 #include <webots/PositionSensor.hpp>
 #include <webots/Motor.hpp>
 #include <iostream>
-using namespace std;
 
+using namespace std;
 using namespace webots;
 
-// This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
-// function(s) and destroys it at the end of the execution.
-// Note that only one instance of Robot should be created in
-// a controller program.
-// The arguments of the main function can be specified by the
-// "controllerArgs" field of the Robot node
 int main(int argc, char **argv) {
-  // create the Robot instance.
+
   Robot *robot = new Robot();
 
-  // get the time step of the current world.
   int timeStep = (int)robot->getBasicTimeStep();
 
   Motor *motor = robot->getMotor("motor");
   PositionSensor *ps = robot->getPositionSensor("encoder");
+
   ps->enable(timeStep);
 
-  // Main loop:
-  // - perform simulation steps until Webots is stopping the controller
+  // Dejar el motor completamente libre
+  motor->setTorque(0.0);
+
   while (robot->step(timeStep) != -1) {
-    // Read the sensors:
-    // Enter here functions to read sensor data, like:
+
     double val = ps->getValue();
-    cout<<"Posicion actual: " <<val<<endl;
-
-    // Process sensor data here.
-
-    // Enter here functions to send actuator commands, like:
-    motor->setPosition(6.28);
-  };
-
-  // Enter here exit cleanup code.
+    cout << "Posicion actual: " << val << endl;
+  }
 
   delete robot;
   return 0;
